7e3d9851f15aace30f2ea197fb83d476ea69c661
jkey20 <helios2sic@gmail.com>
2023-07-02 20:25:33 +0900

    [Refactor] 코드 리팩토링
    - 비어있는 위치 데이터는 중간지점 view로 데이터를 옮기는 과정에서 포함되지 않게 변경
    - Dialog, Marker Util 코드 리팩토링

--- Dutch/app/src/main/java/com/dutch2019/ui/middle/MiddleFragment.kt
+++ Dutch/app/src/main/java/com/dutch2019/ui/middle/MiddleFragment.kt
@@ -2,9 +2,11 @@ package com.dutch2019.ui.middle
 
 import android.app.ProgressDialog
 import android.os.Bundle
+import android.util.Log
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
 import androidx.navigation.findNavController
 import com.dutch2019.BuildConfig
 import com.dutch2019.base.BaseFragment
@@ -12,45 +14,49 @@ import com.skt.Tmap.TMapView
 import dagger.hilt.android.AndroidEntryPoint
 import com.dutch2019.R
 import com.dutch2019.databinding.FragmentMiddleBinding
+import com.dutch2019.model.LocationData
 import com.dutch2019.model.LocationDataList
+import com.dutch2019.ui.main.MainViewModel
+import com.dutch2019.util.dismissLoadingDialog
 import com.dutch2019.util.marker.*
+import com.dutch2019.util.showLoadingDialog
+import com.skt.Tmap.TMapPoint
 import com.skt.Tmap.TMapTapi
 
 @AndroidEntryPoint
 class MiddleFragment : BaseFragment<FragmentMiddleBinding>(
     R.layout.fragment_middle
 ) {
-    private val middleViewModel: MiddleViewModel by activityViewModels()
-
+    private val vm: MiddleViewModel by activityViewModels()
+    private val tMapView by lazy { TMapView(context) }
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        val tmapApi = TMapTapi(requireContext())
-        tmapApi.setSKTMapAuthentication("${BuildConfig.T_MAP_API}")
+
+
+        showLoadingDialog(requireActivity())
 
         MiddleFragmentArgs.fromBundle(requireArguments()).let { data ->
-            middleViewModel.setLocationList(data.locationlist)
+            vm.setLocationList(data.locationlist)
         }
-        val loadingDialog = ProgressDialog(requireActivity())
-        showLoadingDialog(loadingDialog)
-        middleViewModel.setCenterPoint(middleViewModel.calculateCenterPoint(middleViewModel.getLocationList()))
 
-        val tMapView = TMapView(context)
-        markLocationList(tMapView, requireContext(), middleViewModel.getLocationList())
-        markMiddleLocation(tMapView, requireContext(), middleViewModel.getCenterPoint())
-        setMarkRatioLocation(tMapView, requireContext(), middleViewModel.getRatioPoint())
-        setBallonOverlayClickEvent(tMapView, middleViewModel)
+        vm.setCenterPoint(vm.calculateCenterPoint(vm.getLocationList()))
 
-        middleViewModel.setCenterPointAddress(middleViewModel.getCenterPoint())
-        middleViewModel.setCenterPointNearSubway(middleViewModel.getCenterPoint())
+        markLocationList(tMapView, requireContext(), vm.getLocationList())
+        markMiddleLocation(tMapView, requireContext(), vm.getCenterPoint())
+        setMarkRatioLocation(tMapView, requireContext(), vm.getRatioPoint())
+        setBallonOverlayClickEvent(tMapView, vm)
 
-        mapAutoZoom(tMapView, middleViewModel.getLocationList(), middleViewModel.getCenterPoint())
+        vm.setCenterPointAddress(vm.getCenterPoint())
+        vm.setCenterPointNearSubway(vm.getCenterPoint())
+
+        mapAutoZoom(tMapView, vm.getLocationList(), vm.getCenterPoint(),  requireContext())
 
         binding.maplayoutMiddle.addView(tMapView)
 
         binding.buttonMiddleRatiosetting.setOnClickListener { view ->
             view.findNavController().navigate(
                 MiddleFragmentDirections.actionMiddleFragmentToRatioFragment(
-                    LocationDataList().convertLocationData(middleViewModel.getLocationList())
+                    LocationDataList().convertLocationData(vm.getLocationList())
                 )
             )
         }
@@ -58,28 +64,28 @@ class MiddleFragment : BaseFragment<FragmentMiddleBinding>(
         binding.buttonCheckNearfacility.setOnClickListener { view ->
             view.findNavController().navigate(
                 MiddleFragmentDirections.actionMiddleFragmentToNearFragment(
-                    middleViewModel.getCenterPoint().latitude.toFloat(),
-                    middleViewModel.getCenterPoint().longitude.toFloat()
+                    vm.getCenterPoint().latitude.toFloat(),
+                    vm.getCenterPoint().longitude.toFloat()
                 )
             )
 
         }
 
-        middleViewModel.centerPointAddress.observe(viewLifecycleOwner, Observer {
+        vm.centerPointAddress.observe(viewLifecycleOwner, Observer {
             binding.textviewMiddleAddress.text = it
-            middleViewModel.saveLocations(middleViewModel.getLocationList())
-            dismissLoadingDialog(loadingDialog)
+            vm.saveLocations(vm.getLocationList())
+            dismissLoadingDialog()
         })
-        middleViewModel.centerPointNearSubway.observe(viewLifecycleOwner, Observer {
+        vm.centerPointNearSubway.observe(viewLifecycleOwner, Observer {
             binding.textviewMiddleNearsubway.text = it
         })
 
-        middleViewModel.routeTime.observe(viewLifecycleOwner, Observer {
+        vm.routeTime.observe(viewLifecycleOwner, Observer {
             binding.textviewMiddleRoutetime.text = it
         })
     }
 
-    fun setBallonOverlayClickEvent(tMapView: TMapView, viewModel: MiddleViewModel) {
+    private fun setBallonOverlayClickEvent(tMapView: TMapView, viewModel: MiddleViewModel) {
 
         tMapView.setOnMarkerClickEvent { _, p1 ->
             val point = p1.tMapPoint
@@ -105,12 +111,6 @@ class MiddleFragment : BaseFragment<FragmentMiddleBinding>(
         }
 
     }
-    fun showLoadingDialog(loadingDialog : ProgressDialog){
-        loadingDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER)
-        loadingDialog.setMessage("로딩중입니다..")
-        loadingDialog.show()
-    }
-    fun dismissLoadingDialog(loadingDialog : ProgressDialog){
-        loadingDialog.dismiss()
-    }
+
+
 }
--- Dutch/app/src/main/java/com/dutch2019/util/marker/MarkerUtil.kt
+++ Dutch/app/src/main/java/com/dutch2019/util/marker/MarkerUtil.kt
@@ -2,6 +2,7 @@ package com.dutch2019.util.marker
 
 import android.content.Context
 import android.graphics.BitmapFactory
+import android.util.Log
 import androidx.core.content.ContextCompat
 import com.skt.Tmap.TMapPoint
 import com.skt.Tmap.TMapView
@@ -9,37 +10,35 @@ import com.dutch2019.R
 import com.dutch2019.model.LocationData
 import java.lang.Exception
 
-fun markLocationList(tMapView: TMapView, context: Context, locationList : ArrayList<LocationData>){
+fun mark(tMapView: TMapView, context: Context, locationData: LocationData) {
+    val markerItemPoint = TMapPoint(locationData.lat, locationData.lon)
+
+    val markerImage = BitmapFactory.decodeResource(
+        context.resources, R.drawable.ic_marker_black
+    )
+
+
+    val marker = MarkerOverlay(
+        tMapView, context, locationData.name
+    )
+
+    marker.id = locationData.name
+    marker.icon = markerImage
+    marker.setPosition(0.5F, 0.8F)
+    marker.tMapPoint = markerItemPoint
+    tMapView.addMarkerItem2(marker.id, marker)
+}
+
+fun markLocationList(tMapView: TMapView, context: Context, locationList: ArrayList<LocationData>) {
     locationList.forEach { locationData ->
-        val markerItemPoint = TMapPoint(locationData.lat, locationData.lon)
-
-        val markerImage =
-        BitmapFactory.decodeResource(
-            context.resources,
-            R.drawable.ic_marker_black
-        )
-
-
-        val marker = MarkerOverlay(
-            tMapView,
-            context,
-            locationData.name
-        )
-
-        marker.id = locationData.name
-        marker.icon = markerImage
-        marker.setPosition(0.5F, 0.8F)
-        marker.tMapPoint = markerItemPoint
-        tMapView.addMarkerItem2(marker.id, marker)
-
+        mark(tMapView, context, locationData)
     }
 }
-fun markMiddleLocation(tMapView: TMapView, context: Context, centerPoint: TMapPoint){
-    val markerImage =
-        BitmapFactory.decodeResource(
-            context.resources,
-            R.drawable.ic_marker_red
-        )
+
+fun markMiddleLocation(tMapView: TMapView, context: Context, centerPoint: TMapPoint) {
+    val markerImage = BitmapFactory.decodeResource(
+        context.resources, R.drawable.ic_marker_red
+    )
     val marker = MarkerOverlay(tMapView, context, "중간지점")
     val strId = "중간지점"
     marker.id = strId
@@ -51,47 +50,41 @@ fun markMiddleLocation(tMapView: TMapView, context: Context, centerPoint: TMapPo
 }
 
 
-fun mapAutoZoom(tMapView: TMapView, locationList: ArrayList<LocationData>, centerPoint: TMapPoint) {
+fun mapAutoZoom(
+    tMapView: TMapView, locationList: List<LocationData>, centerPoint: TMapPoint, context: Context
+) {
     var leftTopLat = centerPoint.latitude
     var leftTopLon = centerPoint.longitude
     var rightBottomLat = centerPoint.latitude
     var rightBottomLon = centerPoint.longitude
     try {
-        for (i in 0 until locationList.size) {
+        for (i in locationList.indices) {
             if (locationList.isNotEmpty()) {
-                if (locationList[i].lat >= leftTopLat) {
-                    leftTopLat = locationList[i].lat
-                }
-                if (locationList[i].lon >= leftTopLon) {
-                    leftTopLon = locationList[i].lon
-                }
-                if (locationList[i].lat <= rightBottomLat) {
-                    rightBottomLat = locationList[i].lat
-                }
-
-                if (locationList[i].lon <= rightBottomLon) {
-                    rightBottomLon = locationList[i].lon
-                }
+                leftTopLat = checkTopLat(locationList[i], leftTopLat)
+                leftTopLon = checkBottomLon(locationList[i], leftTopLon)
+                rightBottomLat = checkBottomLat(locationList[i], rightBottomLat)
+                rightBottomLon = checkTopLon(locationList[i], rightBottomLon)
             }
 
         }
         val leftTopPoint = TMapPoint(leftTopLat, leftTopLon)
         val rightBottomPoint = TMapPoint(rightBottomLat, rightBottomLon)
+
         tMapView.setCenterPoint(centerPoint.longitude, centerPoint.latitude)
         tMapView.zoomToTMapPoint(leftTopPoint, rightBottomPoint)
+        tMapView.zoomLevel = tMapView.zoomLevel - 1
     } catch (e: Exception) {
         e.printStackTrace()
         tMapView.setCenterPoint(centerPoint.longitude, centerPoint.latitude)
     }
 
 }
+
 fun setMarkRatioLocation(tMapView: TMapView, context: Context, ratioPoint: TMapPoint) {
 
-    val markerImage =
-        BitmapFactory.decodeResource(
-            context.resources,
-            R.drawable.ic_marker_blue
-        )
+    val markerImage = BitmapFactory.decodeResource(
+        context.resources, R.drawable.ic_marker_blue
+    )
 
 
     val marker = MarkerOverlay(tMapView, context, "비율변경지점")
@@ -106,5 +99,40 @@ fun setMarkRatioLocation(tMapView: TMapView, context: Context, ratioPoint: TMapP
     tMapView.addMarkerItem2(strId, marker)
 }
 
-fun getDefaultMarkerBackground(context: Context) = ContextCompat.getDrawable(context, R.drawable.ic_marker_gray)
-fun getChangedtMarkerBackground(context: Context) = ContextCompat.getDrawable(context, R.drawable.ic_marker_gray5)
+fun getDefaultMarkerBackground(context: Context) =
+    ContextCompat.getDrawable(context, R.drawable.ic_marker_gray)
+
+fun getChangedtMarkerBackground(context: Context) =
+    ContextCompat.getDrawable(context, R.drawable.ic_marker_gray5)
+
+fun checkTopLat(locationData: LocationData, lat: Double): Double {
+    return if (locationData.lat >= lat) {
+        locationData.lat
+    } else {
+        lat
+    }
+}
+
+fun checkBottomLat(locationData: LocationData, lat: Double): Double {
+    return if (locationData.lat <= lat) {
+        locationData.lat
+    } else {
+        lat
+    }
+}
+
+fun checkTopLon(locationData: LocationData, lon: Double): Double {
+    return if (locationData.lon >= lon) {
+        locationData.lon
+    } else {
+        lon
+    }
+}
+
+fun checkBottomLon(locationData: LocationData, lon: Double): Double {
+    return if (locationData.lon <= lon) {
+        locationData.lon
+    } else {
+        lon
+    }
+}
