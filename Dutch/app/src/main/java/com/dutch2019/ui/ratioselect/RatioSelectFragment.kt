c8dcd073d54a1979caf36c23568b33f012ca946b
jkey20 <helios2sic@gmail.com>
2023-07-11 17:00:13 +0900

    [Design] RatioFragment, RatioSelectFragment 디자인

---
+++ Dutch/app/src/main/java/com/dutch2019/ui/ratioselect/RatioSelectFragment.kt
@@ -0,0 +1,19 @@
+package com.dutch2019.ui.ratioselect
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.dutch2019.R
+import com.dutch2019.ui.ratio.ARG_PARAM1
+import com.dutch2019.ui.ratio.ARG_PARAM2
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [RatioSelectFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class RatioSelectFragment : Fragment() {
+
+}
