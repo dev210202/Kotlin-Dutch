2402a993db823fe68adba07a8dccd3ab1c34af51
jkey20 <helios2sic@gmail.com>
2023-10-24 13:58:49 +0900

    [Feature] 에러 발생 혹은 안내 메시지 표시를 위한 Toast 기능

--- Dutch/app/src/main/java/com/dutch2019/base/BaseViewModel.kt
+++ Dutch/app/src/main/java/com/dutch2019/base/BaseViewModel.kt
@@ -1,10 +1,16 @@
 package com.dutch2019.base
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.dutch2019.util.Event
 
 abstract class BaseViewModel : ViewModel() {
-    private val _toastVaule = MutableLiveData<String>()
-    val toastValue : MutableLiveData<String> get() = _toastVaule
+    private val _toastValue = MutableLiveData<Event<String>>()
+    val toastValue : LiveData<Event<String>> get() = _toastValue
 
+    fun handleError(throwable: Throwable){
+        val message = throwable.message ?: ""
+        _toastValue.value = Event(message)
+    }
 }
